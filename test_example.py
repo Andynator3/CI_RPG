616cf774ad4cae3c9ad8cc997bdfaee644e2f39a
aurelie31000 <hameras.aurelie@gmail.com>
2025-05-22 12:44:33 +0200

    Ajout du test attaque

--- test_example.py
+++ test_example.py
@@ -2,7 +2,8 @@ import unittest
 
 class TestTrue(unittest.TestCase):
     def test_true_is_true(self):
-        self.assertTrue(True == True)
+        self.assertTrue(True)
 
 if __name__ == '__main__':
     unittest.main()
+
